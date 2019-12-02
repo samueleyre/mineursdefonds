b610c0d1c740d4c98b6d5bf7d109d206a1170797
Samuel Eyre <samuel.eyre@hotmail.fr>
2019-12-02 21:19:35 +0100

    more updaaaaaate

--- src/wp-content/themes/timber/pages/page-home.php
+++ src/wp-content/themes/timber/pages/page-home.php
@@ -5,6 +5,11 @@ $context = Timber::context();
 $timber_post     = new Timber\Post();
 $context['post'] = $timber_post;
 if (class_exists('EM_Events')) {
-	$context['nextDate'] = EM_Events::get( array('limit'=>1,'orderby'=>'start_time') )[0];
+    // todo : if event is defined
+    $EM_Event = EM_Events::get( array('limit'=>1,'orderby'=>'start_time') )[0];
+	$context['nextDateImage'] = $EM_Event->output('#_EVENTIMAGE{150,150}');
+	$context['nextDateLocation'] = $EM_Event->output('#_LOCATIONNAME, #_LOCATIONADDRESS, #_LOCATIONTOWN');
+	$context['nextDateLink'] = $EM_Event->output('#_ATT{link}');
+    $context['nextDate'] = $EM_Event;
 }
 Timber::render( ['pages/home-page.twig'], $context );
