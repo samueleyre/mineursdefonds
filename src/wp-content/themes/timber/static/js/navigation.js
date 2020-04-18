4a11b6277cabdd262ae8e8d075566e2b64083301
Samuel Eyre <samuel.eyre@hotmail.fr>
2020-04-18 16:53:25 +0200

    work on menu

--- src/wp-content/themes/timber/static/js/navigation.js
+++ src/wp-content/themes/timber/static/js/navigation.js
@@ -1,13 +1,24 @@
+let menuActivated = false;
+let subMenuActivated = false;
+
 function rotate() {
     document.getElementById('nav-menu-icon').classList.toggle('nav-menu-icon--open')
 }
 
 function activateMenu() {
     rotate();
+    if (menuActivated) {
+        if (subMenuActivated) {
+            toggleSubMenu();
+        }
+    }
+    menuActivated = !menuActivated;
     document.getElementById('nav-menu-items-wrapper').classList.toggle('nav-menu-items-wrapper--open')
+
 }
 
 function toggleSubMenu() {
+    subMenuActivated = !subMenuActivated;
     document.getElementById('nav-menu-main-items').classList.toggle('nav-menu-main-items--open')
     document.getElementById('nav-menu-sub-items').classList.toggle('nav-menu-sub-items--open')
 }
