91fd809fafb9e4bf428690fab1fef9e61f9ad144
Samuel Eyre <samuel.eyre@hotmail.fr>
2020-04-18 15:26:29 +0200

    projects in menu

--- src/wp-content/themes/timber/static/js/navigation.js
+++ src/wp-content/themes/timber/static/js/navigation.js
@@ -1,15 +1,13 @@
-let menuActivated = false;
-
 function rotate() {
     document.getElementById('nav-menu-icon').classList.toggle('nav-menu-icon--open')
 }
 
 function activateMenu() {
     rotate();
-    menuActivated = !menuActivated;
-    if (menuActivated) {
-        document.getElementById('nav-menu-items').classList.toggle('nav-menu-items--open')
-    } else {
-        document.getElementById('nav-menu-items').classList.toggle('nav-menu-items--open')
-    }
+    document.getElementById('nav-menu-items-wrapper').classList.toggle('nav-menu-items-wrapper--open')
+}
+
+function toggleSubMenu() {
+    document.getElementById('nav-menu-main-items').classList.toggle('nav-menu-main-items--open')
+    document.getElementById('nav-menu-sub-items').classList.toggle('nav-menu-sub-items--open')
 }
